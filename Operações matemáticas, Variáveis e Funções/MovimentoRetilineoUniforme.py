68d6a0be49271752b5e2b34e744940c79bf35d73
thomaz-brito <thomaz28012007@gmail.com>
2026-02-13 10:44:08 -0300

    Create MovimentoRetilineoUniforme.py

---
+++ Operações matemáticas, Variáveis e Funções/MovimentoRetilineoUniforme.py
@@ -0,0 +1,5 @@
+#Faça uma função que calcule a posição de um objeto em movimento retilíneo uniforme em um instante t, com posição inicial s_0 e velocidade v:
+
+def calcula_posicao(t,s_0,v):
+    s=s_0+v*t
+    return s
