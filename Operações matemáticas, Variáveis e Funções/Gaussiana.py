96bf111feccf27458bda01da9d80c9e2e5e4c658
thomaz-brito <thomaz28012007@gmail.com>
2026-02-13 11:13:59 -0300

    Update Gaussiana.py

--- Operações matemáticas, Variáveis e Funções/Gaussiana.py
+++ Operações matemáticas, Variáveis e Funções/Gaussiana.py
@@ -1,1 +1,7 @@
+#Faça uma função que recebe 3 números reais como argumento: x, mi e sigma. Essa função deve retornar o valor da fórmula da gaussiana:
+
+import math
+def calcula_gaussiana(x,mu,sigma):
+    gaussiana=1/(sigma*math.sqrt(2*math.pi))*math.exp(-0.5*((x-mu)/sigma)**2)
+    return gaussiana
 
