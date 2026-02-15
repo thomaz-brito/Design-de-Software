b6400cb8fbd8a8fd162ca07a7b6ba4414943fd84
thomaz-brito <thomaz28012007@gmail.com>
2026-02-14 21:47:32 -0300

    Create ConverterLibras.py

---
+++ Operações matemáticas, Variáveis e Funções/ConverterLibras.py
@@ -0,0 +1,5 @@
+#Faça uma função que recebe um peso em libras e devolve o seu equivalente em quilogramas. Considere 6 casas decimais e utilize a constante 0.45359237.
+
+def libras_para_kg(libras):
+    kg=libras*0.45359237
+    return kg
