b89e79e787614a12a51447b4eda381f1247544d4
thomaz-brito <thomaz28012007@gmail.com>
2026-02-14 21:44:56 -0300

    Create AreaTriangulo.py

---
+++ Operações matemáticas, Variáveis e Funções/AreaTriangulo.py
@@ -0,0 +1,5 @@
+#Faça uma função que calcula a área de um triângulo de base b e altura h, ela deve receber os valores nesta ordem.
+
+def calcula_area_do_triangulo(b,h):
+    area=b*h/2
+    return area
