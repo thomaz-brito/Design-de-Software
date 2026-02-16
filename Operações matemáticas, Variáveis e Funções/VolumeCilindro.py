d7a18cb6bb8bfbeac80b862194ee620e0ef41a9e
Thomaz Brito <thomaz28012007@gmail.com>
2026-02-16 18:24:35 -0300

    Create VolumeCilindro.py

---
+++ Operações matemáticas, Variáveis e Funções/VolumeCilindro.py
@@ -0,0 +1,6 @@
+#Faça uma função que calcule o volume de uma pizza de raio z e altura a. Assuma que a pizza é um cilindro.
+
+import math
+def volume_da_pizza(z,a):
+    volume=math.pi*z**2*a
+    return volume
