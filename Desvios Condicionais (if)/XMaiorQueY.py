91cd4cfe45115888e600de6063ee62803cbcbdfb
Thomaz Brito <thomaz28012007@gmail.com>
2026-03-02 10:22:23 -0300

    Create XMaiorQueY.py

---
+++ Desvios Condicionais (if)/XMaiorQueY.py
@@ -0,0 +1,7 @@
+#Faça uma função que recebe dois números, x e y, como argumentos e devolve 1 se x for maior do que y e 0, caso contrário.
+
+def x_maior_que_y(x,y):
+    if x>y:
+        return 1
+    else:
+        return 0
