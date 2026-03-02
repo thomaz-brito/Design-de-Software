300bb85bc77d9b5c42eb803272821de2452c5053
Thomaz Brito <thomaz28012007@gmail.com>
2026-03-02 10:16:50 -0300

    Create ParOuImpar.py

---
+++ Desvios Condicionais (if)/ParOuImpar.py
@@ -0,0 +1,7 @@
+#Faça uma função que receba um número inteiro como argumento e devolva 1 se esse número for par ou -1 se o número for ímpar.
+
+def verifica_par_ou_impar(x):
+    if x%2==0:
+        return 1
+    else:
+        return -1
