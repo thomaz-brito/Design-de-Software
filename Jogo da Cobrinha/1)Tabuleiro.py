0e8385b3455b2f19aeeefb3ad5d876d5e1e5f871
Thomaz Brito <thomaz28012007@gmail.com>
2026-04-05 14:46:01 -0300

    Update 1)Tabuleiro.py

--- Jogo da Cobrinha/1)Tabuleiro.py
+++ Jogo da Cobrinha/1)Tabuleiro.py
@@ -1,13 +1,5 @@
-#Essa função deve receber um inteiro n e retornar uma matriz n x n (isto é, uma lista de listas) preenchida apenas com o caractere ".".
-#Cada linha do tabuleiro deve ser uma lista.
-#O tabuleiro final deve ser uma lista contendo n linhas.
-#Todas as posições devem ter o caractere "."
-
 def gera_tabuleiro(n):
     tabuleiro=[]
-    elemento=[]
-    for i in range(0,n,1):
-        elemento.append(".")
-    for i in range(0,n,1):
-        tabuleiro.append(elemento)
+    for i in range(0,tamanho,1):
+        tabuleiro.append(["."]*tamanho)
     return tabuleiro
