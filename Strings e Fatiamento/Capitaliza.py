7c1e90ff6dff296c50976eb94c7b9fe91affb469
Thomaz Brito <thomaz28012007@gmail.com>
2026-04-27 11:22:34 -0300

    Create Capitaliza.py

---
+++ Strings e Fatiamento/Capitaliza.py
@@ -0,0 +1,4 @@
+#Escreva uma função chamada capitaliza_string que recebe uma string e devolve a versão capitalizada dessa string, ou seja, devolve a mesma string com a primeira letra maiúscula.
+
+def capitaliza_string(s):
+    return s[0].upper() + s[1::]
